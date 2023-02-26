8df045efcc53c80e5f9e5dc5bef29fe1e8efa1f5
Nearh777 <nearh777@gmail.com>
2023-02-26 09:04:12 +0200

    create elem

--- js/task-02.js
+++ js/task-02.js
@@ -7,6 +7,29 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsRef = document.createElement('li');
-ingredientsRef.classList.add('item');
-console.log(ingredientsRef);
+const ingredientsListRef = document.querySelector('#ingredients');
+
+// const elements = option => {
+//   const ingredientsRef = document.createElement('li');
+//   ingredientsRef.classList.add('item');
+//   ingredientsRef.textContent = option;
+  
+//   return ingredientsRef;
+// };
+
+const makeIngredients = opton => {
+  
+  const ingredientsRef = document.createElement('li');
+  ingredientsRef.classList.add('item');
+  ingredientsRef.textContent = opton;
+  return ingredientsRef;
+};
+
+const elements = makeIngredients(ingredients);
+
+
+ingredientsListRef.append(...elements);
+
+// const ingredientsRef = document.createElement('li');
+// ingredientsRef.classList.add('item');
+// console.log(ingredientsRef);
