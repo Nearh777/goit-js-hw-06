b758fcbeefb6fcc58384cd910510bda065e5c616
Nearh777 <nearh777@gmail.com>
2023-02-26 16:45:18 +0200

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,39 +7,16 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsListEl = document.querySelector('#ingredients');
+const listEl = document.querySelector('#ingredients');
+console.log(listEl);
 
-ingredients.forEach(element) => {
+const ingredientsListEl = ingredients.forEach(element => {
   const makeLiEl = document.createElement('li');
+  console.log(makeLiEl);
   makeLiEl.classList.add('item');
-  makeLiEl.textContent = element;
-  //return makeLiEl;
-};
-ingredientsListEl.appendChild(makeLiEl);
+  makeLiEl.textContent = `${element}`;
+  listEl.append(makeLiEl);
+  console.log(makeLiEl);
+});
 
-
-// const elements = option => {
-//   const ingredientsRef = document.createElement('li');
-//   ingredientsRef.classList.add('item');
-//   ingredientsRef.textContent = option;
-  
-//   return ingredientsRef;
-// };
-
-// const makeIngredients = opton => {
-  
-//   const ingredientsRef = document.createElement('li');
-//   ingredientsRef.classList.add('item');
-//   ingredientsRef.textContent = opton;
-//   return ingredientsRef;
-// };
-
-// const elements = makeIngredients(ingredients);
-
-
-// ingredientsListRef.append(...elements);
-
-// const ingredientsRef = document.createElement('li');
-// ingredientsRef.classList.add('item');
-// console.log(ingredientsRef);
 
