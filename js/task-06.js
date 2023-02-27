67bdb5361dbecabdc9821e3abf7eceb3c10f569d
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-02-27 22:41:50 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,3 +1,6 @@
 const textInput = document.querySelector("#validation-input");
 const setFocusInput = document.querySelector("validation-input.valid");
 const removeFocusInput = document.querySelector("validation-input.valid");
+
+setFocusInput.addEventListener("click", () => {
+  textInput.focus();
