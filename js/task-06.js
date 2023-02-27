3319aed04cace060a4c0b425790a46e5e1a968bd
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-02-27 23:08:27 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,17 @@ const inputEl = document.querySelector("#validation-input");
 // const removeFocusInput = document.querySelector("validation-input.valid");
 
 inputEl.addEventListener("blur", () => {
-  const inputLength = inputEl.dataset.length;
   
-  if (inputLength >= 6) {
-    inputEl.classList.add('valid')
-    inputEl.classList.remove('invalid')
-  } else {
-    inputEl.classList.add('invalid')
-    inputEl.classList.remove('valid')
-  };
+  inputEl.classList.toggle('invalid', inputEl.value.length !== Number(inputEl.getAttribute('data-length')))
+  inputEl.classList.toggle('valid', inputEl.value.length === Number(inputEl.getAttribute('data-length')));
+//   const inputLength = inputEl.dataset.length;
+  
+//   if (inputLength >= 6) {
+//     inputEl.classList.add('valid')
+//     inputEl.classList.remove('invalid')
+//   } else {
+//     inputEl.classList.add('invalid')
+//     inputEl.classList.remove('valid')
+  
   
 });  
