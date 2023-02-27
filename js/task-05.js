9df713f71d38467c480c097aa6105869b4c5f32a
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-02-27 22:05:24 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,7 @@
 const textInput = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
-output.textContent = event.currentTarget.value.trim();
+output.textContent = event.target.value.trim();
 
 if (event.target.value === "") {
 output.textContent = "Anonymous";
