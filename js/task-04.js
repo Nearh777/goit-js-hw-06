286982f0579279924abc228b321b097379ee9482
Nearh777 <nearh777@gmail.com>
2023-02-28 15:35:09 +0200

    finish

--- js/task-04.js
+++ js/task-04.js
@@ -10,15 +10,15 @@
 // counterEl.textContent = counterValue;
 // });
 
-const refs = {
+const links = {
     counterValue: document.querySelector('#value'),
     reduceClickButton: document.querySelector('[data-action="decrement"]'),
     addClickButton: document.querySelector('[data-action="increment"]'),
 };
-refs.reduceClickButton.addEventListener('click', (event) => {
+links.reduceClickButton.addEventListener('click', (event) => {
   refs.counterValue.textContent -= 1;
   });
-refs.addClickButton.addEventListener('click', (event) => {
-let total = parseInt(refs.counterValue.textContent);
-refs.counterValue.textContent = total + 1;
+links.addClickButton.addEventListener('click', (event) => {
+let total = parseInt(links.counterValue.textContent);
+links.counterValue.textContent = total + 1;
 });
--- js/task-06.js
+++ js/task-06.js
@@ -8,21 +8,3 @@ inputEl.classList.remove('valid', 'invalid');
     : inputEl.classList.add('invalid');
  
 });  
-// const setFocusInput = document.querySelector("validation-input.valid");
-// const removeFocusInput = document.querySelector("validation-input.valid");
-
-// inputEl.addEventListener("blur", () => {
-  
-//   inputEl.classList.toggle('invalid', inputEl.value.length !== Number(inputEl.getAttribute('data-length')))
-//   inputEl.classList.toggle('valid', inputEl.value.length === Number(inputEl.getAttribute('data-length')));
-// //   const inputLength = inputEl.dataset.length;
-  
-// //   if (inputLength >= 6) {
-// //    
-// //     inputEl.classList.remove('invalid')
-// //   } else {
-// //     inputEl.classList.add('invalid')
-// //     inputEl.classList.remove('valid')
-  
-  
-// 
