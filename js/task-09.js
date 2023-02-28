5340c6cada5a3bc302b8ad08801aa501f9c5c6cc
Nearh777 <nearh777@gmail.com>
2023-02-28 14:05:18 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const loginFormEl = document.querySelector('.login-form');
+console.log(loginFormEl.querySelector('input[name = email]'));
+loginFormEl.addEventListener('submit', event => {
+  event.preventDefault();
+  const inputEmailEl = loginFormEl.querySelector('input[name = email]');
+  const inputPassEl = loginFormEl.querySelector('input[name = password]');
+  const inputPassElLength = inputPassEl.value.trim().length;
+  const inputEmailElLength = inputEmailEl.value.trim().length;
+  if (inputPassElLength === 0 || inputEmailElLength === 0) {
+    return alert('Всі поля мають бути заповненими!');
+  } else {
+    const elements = {
+      email: inputEmailEl.value,
+      password: inputPassEl.value,
+    };
+    loginFormEl.reset();
+    console.log(elements);
+  }
+});
