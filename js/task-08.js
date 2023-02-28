2dffc9ea688379dc7fa3faf14fa4842933bdf03b
Nearh777 <56692519+Nearh777@users.noreply.github.com>
2023-02-28 06:14:00 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
 
+form.addEventListene('submit', (event) => {
+  const elements: { email, password },
+} = event currenTarget;
+  const inputValue = {
+          email: `${email.value}`, 
+          password: `${password.value}`
+}
+console.log(inputValue);
+if email.value === '' || password.value === '') {
+  window.alert("Всі поля повинні бути заповнені!");
+}
+event.target.reset()
+});
