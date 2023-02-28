1d1b4ab16e40c26e3038b916ddfee0e2b9dc9cef
Nearh777 <nearh777@gmail.com>
2023-02-28 14:24:05 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,11 +1,24 @@
-const btnDecEl = document.querySelector('button[data-action="decrement"]');
-const btnIncEl = document.querySelector('button[data-action="increment"]');
-let counterEl = 0;
-btnDecEl.addEventListener("click", () => {
-counterValue -= 1;
-counterEl.textContent = counterValue;
-});
-btnIncEl.addEventListener("click", () => {
-counterValue += 1;
-counterEl.textContent = counterValue;
+// const btnDecEl = document.querySelector('button[data-action="decrement"]');
+// const btnIncEl = document.querySelector('button[data-action="increment"]');
+// let counterEl = 0;
+// btnDecEl.addEventListener("click", () => {
+// counterValue -= 1;
+// counterEl.textContent = counterValue;
+// });
+// btnIncEl.addEventListener("click", () => {
+// counterValue += 1;
+// counterEl.textContent = counterValue;
+// });
+
+const refs = {
+    counterValue: document.querySelector('#value'),
+    reduceClickButton: document.querySelector('[data-action="decrement"]'),
+    addClickButton: document.querySelector('[data-action="increment"]'),
+};
+refs.reduceClickButton.addEventListener('click', (event) => {
+  refs.counterValue.textContent -= 1;
+  });
+refs.addClickButton.addEventListener('click', (event) => {
+let total = parseInt(refs.counterValue.textContent);
+refs.counterValue.textContent = total + 1;
 });
