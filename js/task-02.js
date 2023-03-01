8cb1ecff78421c9777cae8d1e30cdd090d287a3f
Nearh777 <nearh777@gmail.com>
2023-03-01 12:41:27 +0200

    fixes-1

--- js/task-02.js
+++ js/task-02.js
@@ -10,12 +10,13 @@ const ingredients = [
 const listEl = document.querySelector('#ingredients');
 console.log(listEl);
 
-const ingredientsListEl = ingredients.forEach(element => {
+const ingredientsListEl = ingredients.map(element => {
   const makeLiEl = document.createElement('li');
   console.log(makeLiEl);
   makeLiEl.classList.add('item');
   makeLiEl.textContent = `${element}`;
-  listEl.append(makeLiEl);
-  console.log(makeLiEl);
+  return makeLiEl;
+  
 });
-
+console.log(ingredientsListEl);
+listEl.append(...ingredientsListEl);
--- js/task-04.js
+++ js/task-04.js
@@ -1,24 +1,25 @@
-// const btnDecEl = document.querySelector('button[data-action="decrement"]');
-// const btnIncEl = document.querySelector('button[data-action="increment"]');
-// let counterEl = 0;
-// btnDecEl.addEventListener("click", () => {
-// counterValue -= 1;
-// counterEl.textContent = counterValue;
-// });
-// btnIncEl.addEventListener("click", () => {
-// counterValue += 1;
-// counterEl.textContent = counterValue;
-// });
-
-const links = {
-    counterValue: document.querySelector('#value'),
-    reduceClickButton: document.querySelector('[data-action="decrement"]'),
-    addClickButton: document.querySelector('[data-action="increment"]'),
-};
-links.reduceClickButton.addEventListener('click', (event) => {
-  refs.counterValue.textContent -= 1;
+const btnDecEl = document.querySelector('button[data-action="decrement"]');
+const btnIncEl = document.querySelector('button[data-action="increment"]');
+let counterEl = document.querySelector("#value");
+  let counterValue = 0;
+  btnDecEl.addEventListener("click", () => {
+    counterValue -= 1;
+    counterEl.textContent = counterValue;
   });
-links.addClickButton.addEventListener('click', (event) => {
-let total = parseInt(links.counterValue.textContent);
-links.counterValue.textContent = total + 1;
-});
+  btnIncEl.addEventListener("click", () => {
+    counterValue += 1;
+    counterEl.textContent = counterValue;
+  });
+
+// const links = {
+//     counterValue: document.querySelector('#value'),
+//     reduceClickButton: document.querySelector('[data-action="decrement"]'),
+//     addClickButton: document.querySelector('[data-action="increment"]'),
+// };
+// links.reduceClickButton.addEventListener('click', (event) => {
+//   refs.counterValue.textContent -= 1;
+//   });
+// links.addClickButton.addEventListener('click', (event) => {
+// let total = parseInt(links.counterValue.textContent);
+// links.counterValue.textContent = total + 1;
+// });
