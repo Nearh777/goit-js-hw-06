58403f25e88c019fe130e419d0d5198b6c71a3d8
Nearh777 <nearh777@gmail.com>
2023-03-01 17:54:57 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,24 @@
+// const inputEl = document.querySelector('#validation-input');
+// const validValue = inputEl.getAttribute('data-length');
+
+// inputEl.addEventListener("blur", () => {
+// inputEl.classList.remove('valid', 'invalid');
+//   inputEl.value.length === Number(validValue) 
+//     ? inputEl.classList.add('valid')
+//     : inputEl.classList.add('invalid');
+ 
+// });  
+
 const inputEl = document.querySelector('#validation-input');
 const validValue = inputEl.getAttribute('data-length');
 
 inputEl.addEventListener("blur", () => {
 inputEl.classList.remove('valid', 'invalid');
-  inputEl.value.length === Number(validValue) 
-    ? inputEl.classList.add('valid')
-    : inputEl.classList.add('invalid');
+  if(inputEl.value.length === Number(validValue)) {
+    inputEl.classList.add('valid');
+  } else {
+    inputEl.classList.add('invalid');
+  }
+   
  
 });  
